8c373a51e817e185902ba6e216f1be7852bb02b9
hira_ <hira_@Md-Chowdhury>
2022-01-06 18:03:55 -0600

    final commit for HW 11

--- src/test/java/com/noorteck/java/hw11/Subtraction.java
+++ src/test/java/com/noorteck/java/hw11/Subtraction.java
@@ -1,33 +1,31 @@
 package com.noorteck.java.hw11;
 
 public class Subtraction {
-	
+
 	public static void main(String[] args) {
-		int diffTotal = difference21(17);
+		int diffTotal = difference21(10);
 		System.out.println(diffTotal);
 	}
-	
-	
-	
+
 	public static int difference21(int number) {
 		int result = 0;
-		if(number == 21) {
+		if (number < 21) { 				// if number is equal to 21 then the value will be 0
 			result = (number - 21);
-		}else if (number >21) {
-			result =(number - 21) ;// unable to create the logic
-		
+		} else if (number > 21) { 			// if the number is greater than 21 than the result value will double
+			result = ((21 - number) * 2);	// unable to create the logic
+
+		} else {
+			result = 0;
 		}
-		
-		
+
 		return result;
 	}
 }
 /**
-1 parameter
-• Method Name: difference21
-• Parameter 1: int number
-• Method Return Type: int
-Given an int number, return the difference between variable number and 21 (number-21), except return double the difference if n is over 21.
-Then call your function from main method: Create an int variable in your main method and assign the value what difference21method returns
-• Variable Name: int diffTotal;
-*/
+ * 1 parameter • Method Name: difference21 • Parameter 1: int number • Method
+ * Return Type: int Given an int number, return the difference between variable
+ * number and 21 (number-21), except return double the difference if n is over
+ * 21. Then call your function from main method: Create an int variable in your
+ * main method and assign the value what difference21method returns • Variable
+ * Name: int diffTotal;
+ */
